8dcf49feb60f2c0e2b66eae603ba9c99f684073e
Richardsen Thomas <89308520+rtomswastaken@users.noreply.github.com>
2026-03-30 22:10:19 +0530

    Create initial To Do List app with Tkinter
    Initialize a basic To Do List application with a GUI.

---
+++ ToDo_LIST.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+root=tk.Tk()
+root.title("To Do List App")
+root.geometry("650x450")
+
+titleLabel=tk.Label(root,text="ToDo List")
+titleLabel.pack()
+
+entryBox=
+root.mainloop()
